8acd3ce7907186223964e46cc57a5f76a65dcebf
ArtemKarlov <ar.karlov@gmial.com>
2020-05-19 17:34:58 +0400

    add first game

--- scripts/guessingGame2.js
+++ scripts/guessingGame2.js
@@ -1,40 +1,59 @@
+	// let answer = parseInt(Math.random()*100);
+	// console.log(answer);
+	// let user1Answer = +prompt("Игрок 1, Угадайте число от 0 до 100");
+	// let user2Answer = +prompt("Игрок 2, Угадайте число от 0 до 100");
+	// let maxTryCount = 3;
+	// for (let tryCount = 1; tryCount <= maxTryCount; tryCount++){
+	// 	if ((user1Answer == answer) && (user2Answer != answer)) {
+	// 		alert('Игрок 1, Вы победили');
+	// 		break;
+	// 	} else if ((user2Answer == answer) && (user1Answer != answer)) {
+	// 		alert('Игрок 2, Вы победили');
+	// 		break;
+	// 	} else if ((user1Answer < answer) && (user2Answer < answer)){
+	// 		alert('Игрок 1, Мало\nИгрок 2, Мало');
+	// 	} else if ((user1Answer < answer) && (user2Answer > answer)){
+	// 		alert('Игрок 1, Мало\nИгрок 2, Много');
+	// 	} else if ((user1Answer > answer) && (user2Answer < answer)){
+	// 		alert('Игрок 1, Много\nИгрок 2, Мало');
+	// 	} else if ((user1Answer > answer) && (user2Answer > answer)){
+	// 		alert('Игрок 1, Много\nИгрок 2, Много');
+	// 	}
+
+	// 	if (tryCount == maxTryCount){
+	// 		alert('Вы проиграли');
+	// 		break;
+	// 	}
+
+	// 	if (!(confirm('Игрок 1, Хотите пролоджить игру?'))){
+	// 		alert('Игрок 1 прекратил игру');
+	// 		break;
+	// 	} else if (!(confirm('Игрок 2, Хотите пролоджить игру?'))){
+	// 		alert('Игрок 2 прекратил игру');
+	// 		break;
+	// 	}
+
+	// 	user1Answer = +prompt("Игрок 1, Угадайте число от 0 до 100 \n Осталось " + (maxTryCount - tryCount) + ' попыток');
+	// 	user2Answer = +prompt("Игрок 2, Угадайте число от 0 до 100 \n Осталось " + (maxTryCount - tryCount) + ' попыток');
+	// }
+
+	// alert('Правильный ответ: ' + answer);
+
+
 	let answer = parseInt(Math.random()*100);
 	console.log(answer);
-	let user1Answer = +prompt("Игрок 1, Угадайте число от 0 до 100");
-	let user2Answer = +prompt("Игрок 2, Угадайте число от 0 до 100");
-	let maxTryCount = 3;
-	for (let tryCount = 1; tryCount <= maxTryCount; tryCount++){
-		if ((user1Answer == answer) && (user2Answer != answer)) {
-			alert('Игрок 1, Вы победили');
-			break;
-		} else if ((user2Answer == answer) && (user1Answer != answer)) {
-			alert('Игрок 2, Вы победили');
-			break;
-		} else if ((user1Answer < answer) && (user2Answer < answer)){
-			alert('Игрок 1, Мало\nИгрок 2, Мало');
-		} else if ((user1Answer < answer) && (user2Answer > answer)){
-			alert('Игрок 1, Мало\nИгрок 2, Много');
-		} else if ((user1Answer > answer) && (user2Answer < answer)){
-			alert('Игрок 1, Много\nИгрок 2, Мало');
-		} else if ((user1Answer > answer) && (user2Answer > answer)){
-			alert('Игрок 1, Много\nИгрок 2, Много');
-		}
 
-		if (tryCount == maxTryCount){
-			alert('Вы проиграли');
-			break;
-		}
+	function checkAnsewrs2() {
+		let user1Answer = +document.getElementById("user1Answer").value;
+		document.getElementById("test").innerHTML = user1Answer;
+		// document.getElementById("qustion1").innerHTML
 
-		if (!(confirm('Игрок 1, Хотите пролоджить игру?'))){
-			alert('Игрок 1 прекратил игру');
-			break;
-		} else if (!(confirm('Игрок 2, Хотите пролоджить игру?'))){
-			alert('Игрок 2 прекратил игру');
-			break;
+		if (user1Answer == answer) {
+			console.log('ok');
 		}
-
-		user1Answer = +prompt("Игрок 1, Угадайте число от 0 до 100 \n Осталось " + (maxTryCount - tryCount) + ' попыток');
-		user2Answer = +prompt("Игрок 2, Угадайте число от 0 до 100 \n Осталось " + (maxTryCount - tryCount) + ' попыток');
+		else {
+			console.log('not ok');
+		}
 	}
-
-	alert('Правильный ответ: ' + answer);
+	
+	
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,68 +1,110 @@
-// 3. Доработать игру в загадки:
-//     a. Избавиться в игре в загадки от дублирования кода, используя массивы и функции.
-//     b. Сделать так, чтобы программа поддерживала несколько правильных ответов на один и тот же вопрос. Все возможные варианты задаются программистом в коде программы.
-	function askQuestion(questionArray) {
-		let userAnswer = (prompt(questionArray[0] + '?')).toLowerCase();
-		let isRight = false;
+// // 3. Доработать игру в загадки:
+// //     a. Избавиться в игре в загадки от дублирования кода, используя массивы и функции.
+// //     b. Сделать так, чтобы программа поддерживала несколько правильных ответов на один и тот же вопрос. Все возможные варианты задаются программистом в коде программы.
+// 	function askQuestion(questionArray) {
+// 		let userAnswer = (prompt(questionArray[0] + '?')).toLowerCase();
+// 		let isRight = false;
 	
-		for (let i = 1; i < questionArray.length; i++) {
-			if (userAnswer == questionArray[i]) {
-				isRight = true;
-				break;
-			}
-		}
-		if (isRight) {
-			alert('Правильно');
-			return 1;
-		} else {
-			alert('Неверно');
-			return 0;
-		}
-	}
+// 		for (let i = 1; i < questionArray.length; i++) {
+// 			if (userAnswer == questionArray[i]) {
+// 				isRight = true;
+// 				break;
+// 			}
+// 		}
+// 		if (isRight) {
+// 			alert('Правильно');
+// 			return 1;
+// 		} else {
+// 			alert('Неверно');
+// 			return 0;
+// 		}
+// 	}
 
 
-		function runQuiz(question, answersArray) {
-			let userAnswer = (prompt(question + '?')).toLowerCase();
-			let isRight = false;
+// 		function runQuiz(question, answersArray) {
+// 			let userAnswer = (prompt(question + '?')).toLowerCase();
+// 			let isRight = false;
 		
-			for (let i = 0; i < answersArray.length; i++) {
-				if (userAnswer == answersArray[i]) {
-					isRight = true;
-					break;
-				}
-			}
-			if (isRight) {
-				alert('Правильно');
-				return 1;
-			} else {
-				alert('Неверно');
-				return 0;
-			}
-		}
+// 			for (let i = 0; i < answersArray.length; i++) {
+// 				if (userAnswer == answersArray[i]) {
+// 					isRight = true;
+// 					break;
+// 				}
+// 			}
+// 			if (isRight) {
+// 				alert('Правильно');
+// 				return 1;
+// 			} else {
+// 				alert('Неверно');
+// 				return 0;
+// 			}
+// 		}
 
 
-	let rightAnswerCount = 0;
-	//для функции askQuestion(questionArray)
-	// let questions = [
-	// 	['столица России', 'москва', 'moscow', 'vjcrdf', 'ьщысщц'],
-	// 	['столица Германии', 'берлин', 'berlin', ',thkby', 'иукдшт'],
-	// 	['столица США', 'вашингтон', 'washington', 'dfibyunjy', 'цфырштпещт'],
-	// 	['столица Великобритании', 'лондон', 'london', 'kjyljy'],
-	// ];
+// 	let rightAnswerCount = 0;
+// 	//для функции askQuestion(questionArray)
+// 	// let questions = [
+// 	// 	['столица России', 'москва', 'moscow', 'vjcrdf', 'ьщысщц'],
+// 	// 	['столица Германии', 'берлин', 'berlin', ',thkby', 'иукдшт'],
+// 	// 	['столица США', 'вашингтон', 'washington', 'dfibyunjy', 'цфырштпещт'],
+// 	// 	['столица Великобритании', 'лондон', 'london', 'kjyljy'],
+// 	// ];
 
-	// for (let i = 0; i < questions.length; i++){
-	// 	rightAnswerCount = rightAnswerCount + askQuestion(questions[i]);
-	// }
+// 	// for (let i = 0; i < questions.length; i++){
+// 	// 	rightAnswerCount = rightAnswerCount + askQuestion(questions[i]);
+// 	// }
 	
-	let questions = [
-		['столица России', ['москва', 'moscow', 'vjcrdf', 'ьщысщц']],
-		['столица Германии', ['берлин', 'berlin', ',thkby', 'иукдшт']],
-		['столица США', ['вашингтон', 'washington', 'dfibyunjy', 'цфырштпещт']],
-		['столица Великобритании', ['лондон', 'london', 'kjyljy']],
-	];
+// 	let questions = [
+// 		['столица России', ['москва', 'moscow', 'vjcrdf', 'ьщысщц']],
+// 		['столица Германии', ['берлин', 'berlin', ',thkby', 'иукдшт']],
+// 		['столица США', ['вашингтон', 'washington', 'dfibyunjy', 'цфырштпещт']],
+// 		['столица Великобритании', ['лондон', 'london', 'kjyljy']],
+// 	];
 
-	for (let i = 0; i < questions.length; i++){
-		rightAnswerCount = rightAnswerCount + runQuiz(questions[i][0], questions[i][1]);
+// 	for (let i = 0; i < questions.length; i++){
+// 		rightAnswerCount = rightAnswerCount + runQuiz(questions[i][0], questions[i][1]);
+// 	}
+
+// 	alert('Игра окончена, правильных ответов: '+ rightAnswerCount);
+
+let questions = [
+    ['столица России', ['москва', 'moscow', 'vjcrdf', 'ьщысщц']],
+    ['столица Германии', ['берлин', 'berlin', ',thkby', 'иукдшт']],
+    ['столица США', ['вашингтон', 'washington', 'dfibyunjy', 'цфырштпещт']],
+    ['столица Великобритании', ['лондон', 'london', 'kjyljy']],
+];
+let score = 0;
+
+function checkAnswer(inputId, answersArray) {
+	let userAnswer = document.getElementById(inputId).value;
+	userAnswer = userAnswer.toLowerCase();
+
+	for (let i = 0; i < answersArray.length; i++) {
+		if (userAnswer == answersArray[i]) {
+			score++
+			break;
+		}
 	}
+}
 
-	alert('Игра окончена, правильных ответов: '+ rightAnswerCount);
+function checkAnswers() {
+   checkAnswer("userAnswer1", questions[0][1]);
+   checkAnswer("userAnswer2", questions[1][1]);
+   checkAnswer("userAnswer3", questions[2][1]);
+   checkAnswer("userAnswer4", questions[3][1]);
+
+   document.getElementById("score").innerHTML = score;
+
+   var inputs = document.querySelectorAll('input[type=text]');
+	for (var i = 0;  i < inputs.length; i++) {
+		inputs[i].value = '';
+	};
+
+	score = 0;
+
+}
+
+document.getElementById("qustion1").innerHTML = questions[0][0];
+document.getElementById("qustion2").innerHTML = questions[1][0];
+document.getElementById("qustion3").innerHTML = questions[2][0];
+document.getElementById("qustion4").innerHTML = questions[3][0];
